16b88f516f2e794b8a4be65d3032cfab9e95cb8c
Carlos Cante <2430897080101@ingenieria.usac.edu.gt>
2021-09-04 23:11:03 -0600

    practica 3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();
 app.get('/', function (req, res) {
- res.send('Hola mundo');
+ res.send('Hola mundo, practica 3');
 });
 
 app.listen(process.env.PORT || 3000);
