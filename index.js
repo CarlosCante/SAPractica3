ddc5e923f720e7d7017839507878e20673825a12
Carlos Cante <2430897080101@ingenieria.usac.edu.gt>
2021-10-23 19:15:05 -0600

    p

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();
 app.get('/', function (req, res) {
- res.send('Hola mundo, practica 4.');
+ res.send('Hola mundo, practica 6');
 });
 
 app.listen(process.env.PORT || 3000);
