d4682c0405f7ad774daf0089a3e61884e9bf1a6c
Carlos Cante <2430897080101@ingenieria.usac.edu.gt>
2021-10-23 20:48:25 -0600

    PRUEBA

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();
 app.get('/', function (req, res) {
- res.send('Hola mundo, practica 6');
+ res.send('Hola mundo, practica 6, prueba del dia');
 });
 
 app.listen(process.env.PORT || 3000);
